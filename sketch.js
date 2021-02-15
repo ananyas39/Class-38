63b052fecc5500940ce2900c1aecc5c6d55c7858
Ananya <ananya2007.singh@gmail.com>
2021-02-15 17:14:36 +0530

    Structure before Coding

--- sketch.js
+++ sketch.js
@@ -1,60 +1,25 @@
-var ball;
 var database;
-var position;
 
+var gameState = 0;
+var playerCount;
+
+var form, game, player;
 function setup(){
     
-    createCanvas(500,500);
-
+    createCanvas(400,400);
     database = firebase.database();
-    console.log(database);
-   
-    ball = createSprite(100,100,10,10);
-    ball.shapeColor = "purple";
 
-    var ballPosition = database.ref('ball/position');
-    ballPosition.on("value",readPosition,showError);
+    game = new Game();
+    game.getState();
+    game.start();
+   
 
 }
 
 function draw(){
     
-    background("pink");
+    background("orange");
 
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function writePosition(x,y){
-
-    database.ref('ball/position').set({
-      
-        'x': position.x + x,
-        'y': position.y + y
-        
-
-    })
     
-}
 
-function readPosition(data){
-position = data.val();
-
-ball.x = position.x;
-ball.y = position.y;
-
-}
-function showError(){
-    console.log("There is problem in reading value from database");
 }
