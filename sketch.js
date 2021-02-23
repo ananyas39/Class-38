3e4d88f6972a607cf91939690e6322ab682d3909
Ananya <ananya2007.singh@gmail.com>
2021-02-23 18:19:32 +0530

    Car Sprites Added

--- sketch.js
+++ sketch.js
@@ -1,35 +1,31 @@
-var database;
+var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
 var allPlayers;
+var distance = 0;
+var database;
 
-var form, game, player;
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+
 function setup(){
-    
-    createCanvas(400,400);
-    database = firebase.database();
-
-    game = new Game();
-    game.getState();
-    game.start();
-   
-
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
 
+
 function draw(){
-
-    background("pink");
-
-    if(playerCount === 4){
-        game.update(1);
-    }
-    
-    if(gameState === 1){
-        clear();
-        game.play();
-    }
-
-    
-
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
 }
